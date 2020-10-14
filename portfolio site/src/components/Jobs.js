cb8c199eb813bdd2faad25a43d5dc3ee022ace49
sadewole <samueladewole15@gmail.com>
2020-10-14 11:51:31 +0100

    portfolio home, projects page done

--- portfolio site/src/components/Jobs.js
+++ portfolio site/src/components/Jobs.js
@@ -1,11 +1,68 @@
-import React from "react"
+import React, { useState } from "react"
 import Title from "./Title"
 import { FaAngleDoubleRight } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
 
+const query = graphql`
+  {
+    allStrapiJobs(sort: { fields: strapiId, order: DESC }) {
+      nodes {
+        company
+        position
+        date
+        strapiId
+        desc {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+
 const Jobs = () => {
-  return <h2>jobs component</h2>
+  const {
+    allStrapiJobs: { nodes: jobs },
+  } = useStaticQuery(query)
+  const [value, setValue] = useState(0)
+  const { company, position, date, desc } = jobs[value]
+
+  return (
+    <section className="section jobs">
+      <Title title="experience" />
+      <div className="jobs-center">
+        {/** btn container */}
+        <div className="btn-container">
+          {jobs.map((job, index) => {
+            return (
+              <button
+                key={job.strapiId}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && "active-btn"}`}
+              >
+                {job.company}
+              </button>
+            )
+          })}
+        </div>
+        <article className="job-info">
+          <h3>{position}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{date}</p>
+          {desc.map(item => (
+            <div key={item.id} className="job-desc">
+              <FaAngleDoubleRight className="job-icon" />
+              <p>{item.name}</p>
+            </div>
+          ))}
+        </article>
+      </div>
+      <Link to="/about" className="btn center-btn">
+        more info
+      </Link>
+    </section>
+  )
 }
 
 export default Jobs
--- portfolio site/src/pages/index.js
+++ portfolio site/src/pages/index.js
@@ -6,7 +6,42 @@ import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
-export default () => {
-  return <h2>index page</h2>
+
+export default ({ data }) => {
+  const {
+    allStrapiProjects: { nodes: projects },
+  } = data
+  return (
+    <Layout>
+      <Hero />
+      <Services />
+      <Jobs />
+      <Projects projects={projects} title="featured projects" showLink />
+    </Layout>
+  )
 }
-// ...GatsbyImageSharpFluid
+
+export const query = graphql`
+  {
+    allStrapiProjects(filter: { featured: { eq: true } }) {
+      nodes {
+        description
+        id
+        url
+        title
+        gitthub
+        stack {
+          id
+          title
+        }
+        image {
+          childImageSharp {
+            fluid(grayscale: true) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
