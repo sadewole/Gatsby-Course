07c6aeb831f82bb410ff883e4ecfc6e2db319719
sadewole <samueladewole15@gmail.com>
2020-11-20 21:22:34 +0100

    build project

--- portfolio site/src/components/SEO.js
+++ portfolio site/src/components/SEO.js
@@ -2,8 +2,47 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-const SEO = () => {
-  return <div>SEO Component</div>
+const SEO = ({ title, description }) => {
+  const {
+    site: {
+      siteMetadata: {
+        author,
+        image,
+        siteDesc,
+        siteUrl,
+        siteTitle,
+        twitterUsername,
+      },
+    },
+  } = useStaticQuery(query)
+  return (
+    <Helmet title={`${title} | ${siteTitle}`} htmlAttributes={{ lang: "en" }}>
+      <meta name="description" content={description || siteDesc} />
+      <meta name="image" content={image} />
+      {/** twitter card */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={twitterUsername} />
+      <meta name="twitter:title" content={siteTitle} />
+      <meta name="twitter:description" content={siteDesc} />
+      <meta name="twitter:url" content={`${siteUrl}${image}`} />
+      <meta name="twitter:author" content={author} />
+    </Helmet>
+  )
 }
+
+const query = graphql`
+  {
+    site {
+      siteMetadata {
+        author
+        image
+        siteDesc: description
+        siteUrl
+        siteTitle: title
+        twitterUsername
+      }
+    }
+  }
+`
 
 export default SEO
--- portfolio site/src/pages/about.js
+++ portfolio site/src/pages/about.js
@@ -3,6 +3,7 @@ import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Title from "../components/Title"
 import Image from "gatsby-image"
+import SEO from "../components/SEO"
 
 const About = ({
   data: {
@@ -12,6 +13,7 @@ const About = ({
   const { title, info, stack, image } = nodes[0]
   return (
     <Layout>
+      <SEO title="About" description="This is the about page" />
       <section className="about-page">
         <div className="section-center about-center">
           <Image fluid={image.childImageSharp.fluid} className="about-img" />
